7e1dadb5282177a87c2c05759e259ce4dadb8b8d
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-03-24 13:33:51 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!',
+            style: GoogleFonts.poppins(
+              fontSize: 32,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
