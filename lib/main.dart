98800d54f7b1dc59f03b4bde958f80d957cb1b27
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-03-27 14:46:50 +0600

    code for acceing second scrren from first screen link

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Styling the texts',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      title: 'STRAWHATS',
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Straw Hats Crew'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CachedNetworkImage(
+              imageUrl:
+                  "https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/11/Straw-Hat-Crew-Pirates-of-One-Piece-Featured-Image.png",
+              width: 200,
+              height: 200,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget:
+                  (context, url, error) => const Icon(Icons.error, size: 50),
             ),
-          ),
-          centerTitle: true,
+            const SizedBox(height: 20),
+            const Text(
+              'This are the strawhats ',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+              child: const Text('See Straw Hats Ship'),
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Large Text with Bold and Red color
-              Text(
-                'Giants were always larger than humans'
-                    ' so this is a large text',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                  height: 5,
-                ),
-              ),
-              SizedBox(height: 10),
+      ),
+    );
+  }
+}
 
-              // Medium Text with Italic style
-              Text(
-                'I always wondered if italic font was named after Italy'
-                    ' So this is Italic font',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.blue,
-                  height: 5,
-                ),
-              ),
-              SizedBox(height: 10),
-
-              // Small Text with normal style
-              Text(
-                'We are really small if compared to the universe'
-                    ' so this is small text ig!',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.green,
-                  height: 5,
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Using Row and allign texts vertically
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Monkey D Luffy',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.purple,
-                    ),
-                  ),
-                  Text(
-                    'Is the reincarnation of NIKA',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.orange,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-
-              // Text with Shadow effect
-              Text(
-                'My first In_Game name was Shadow'
-                    ' PRIME_Shadow',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  height: 2.5,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10.0,
-                      color: Colors.grey,
-                      offset: Offset(2.0, 2.0),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ship'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CachedNetworkImage(
+              imageUrl:
+                  "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/one-piece-the-next-ship-of-the-straw-hat-pirates.JPG",
+              width: 200,
+              height: 200,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget:
+                  (context, url, error) => const Icon(Icons.error, size: 50),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'This is the ship called The Thousand Sunny',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('To the crew'),
+            ),
+          ],
         ),
       ),
     );
