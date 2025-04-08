acbe2d27be68e4261be12b0969af49556707ec76
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-04-08 18:38:24 +0600

    image in grid mode and titles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,83 +12,100 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Login Page',
-      home: LoginScreen(),
+      title: 'Straw Hats Grid',
+      home: ImageGridScreen(),
     );
   }
 }
-
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('You have logged in Successfully!')),
-      );
+class ImageGridScreen extends StatelessWidget {
+  final List<Map<String, String>> strawHats = [
+    {
+      "name": "Monkey D. Luffy",
+      "image": "https://miro.medium.com/v2/resize:fit:736/1*YqfVlyCe06DfcPsR3kpYrw.jpeg"
+    },
+    {
+      "name": "Roronoa Zoro",
+      "image": "https://i.imgur.com/FhjJZpq.jpeg"
+    },
+    {
+      "name": "Sanji",
+      "image": "https://i.imgur.com/KkXM3Fn.jpeg"
+    },
+    {
+      "name": "Nami",
+      "image": "https://i.imgur.com/30BEiZT.jpeg"
+    },
+    {
+      "name": "Usopp",
+      "image": "https://i.imgur.com/FZ9AYmU.jpeg"
+    },
+    {
+      "name": "Tony Tony Chopper",
+      "image": "https://i.imgur.com/53QSoWJ.jpeg"
+    },
+    {
+      "name": "Nico Robin",
+      "image": "https://i.imgur.com/A9q5DAH.jpeg"
+    },
+    {
+      "name": "Franky",
+      "image": "https://i.imgur.com/xoKmmpO.jpeg"
+    },
+    {
+      "name": "Brook",
+      "image": "https://i.imgur.com/BMqyr3M.jpeg"
+    },
+    {
+      "name": "Jinbei",
+      "image": "https://i.imgur.com/cjOZ0vi.jpeg"
     }
-  }
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Straw Hats Crew'),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.red,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter username';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter password';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: const Text('Login'),
-              ),
-            ],
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // 2 items per row
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 0.8, // Controls height of images
           ),
+          itemCount: strawHats.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: CachedNetworkImage(
+                      imageUrl: strawHats[index]["image"]!,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => const Icon(Icons.error, size: 50),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      strawHats[index]["name"]!,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
