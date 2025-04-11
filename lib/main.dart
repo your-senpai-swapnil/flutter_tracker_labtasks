ccbc4295cd30348fa6e9242df64d66d197d3cc17
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-04-11 18:50:09 +0600

    local assets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyBottomNavApp());
-}
+void main() => runApp(MyApp());
 
-class MyBottomNavApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bottom Navigation Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BottomNavScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class BottomNavScreen extends StatefulWidget {
-  @override
-  _BottomNavScreenState createState() => _BottomNavScreenState();
-}
-
-class _BottomNavScreenState extends State<BottomNavScreen> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = [
-    HomeTab(),
-    SearchTab(),
-    ProfileTab(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(_getTitle())),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-    );
-  }
-
-  String _getTitle() {
-    switch (_currentIndex) {
-      case 0:
-        return 'Home';
-      case 1:
-        return 'Search';
-      case 2:
-        return 'Profile';
-      default:
-        return '';
-    }
-  }
-}
-class HomeTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/first.png', width: 150),
-          SizedBox(height: 10),
-          Text('Welcome to the Home Tab', style: TextStyle(fontSize: 18)),
-        ],
-      ),
-    );
-  }
-}
-
-class SearchTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.search, size: 80, color: Colors.grey),
-          SizedBox(height: 10),
-          Text('Search your content', style: TextStyle(fontSize: 18)),
-        ],
-      ),
-    );
-  }
-}
-
-class ProfileTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundImage: AssetImage('assets/images/luffy.jpeg'),
-            radius: 50,
-          ),
-          SizedBox(height: 10),
-          Text('Luffy D. Monkey', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          Text('King of the Pirates', style: TextStyle(color: Colors.grey)),
-        ],
+      title: 'Strawhats',
+      home: Scaffold(
+        appBar: AppBar(title: Text('strawhats')),
+        body: Center(child: Image.asset('assets/11.jpeg')),
       ),
     );
   }
