4cfe6afaa1e336150b7480d86d851e680d537c2b
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-04-11 18:56:50 +0600

    custom container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Strawhats',
+      title: 'Custom Container',
       home: Scaffold(
-        appBar: AppBar(title: Text('strawhats')),
-        body: Center(child: Image.asset('assets/11.jpeg')),
+        appBar: AppBar(
+          title: Text('Styled Container added'),
+        ),
+        body: Center(
+          child: FancyContainer(),
+        ),
+      ),
+    );
+  }
+}
+
+class FancyContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Text(
+        'yo!Genki dana na!',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
